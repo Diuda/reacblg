4d4b1e9105566fe20beaa040caa549f4bfbd239a
Diuda <divyanshu.1996@gmail.com>
2019-01-10 17:43:16 +0530

    added saga middleware

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { reducers } from "../reducers/index";
 
-const store = createStore(reducers);
+import createSagaMiddleware from "redux-saga";
+
+import { sagas } from "../saga/index";
+
+
+const sagaMiddleware= createSagaMiddleware();
+
+const reduxDevTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+
+const store = createStore(reducers,
+    compose(applyMiddleware(sagaMiddleware), reduxDevTools));
+
+sagaMiddleware.run(sagas);
+
 
 export default store;
