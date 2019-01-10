4dd79f23ef18ba937a150dda1c6367b55c4bac0f
Diuda <divyanshu.1996@gmail.com>
2019-01-10 17:43:02 +0530

    added root saga

--- src/js/saga/index.js
+++ src/js/saga/index.js
@@ -1,3 +1,10 @@
 import { takeLatest } from "redux-saga";
 import { fork } from "redux-saga/effects";
 import { layerFetchList, layerAdd } from "./layer";
+
+
+export function* sagas() {
+    yield [
+        fork(takeLatest,"LAYER_ADD", layerAdd)
+    ]
+}
