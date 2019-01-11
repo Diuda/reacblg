0c0798e6b8097bfc13988bb6c07764e7745135a0
Diuda <divyanshu.1996@gmail.com>
2019-01-11 19:25:30 +0530

    success action added

--- src/js/saga/layer.js
+++ src/js/saga/layer.js
@@ -1,20 +1,34 @@
-
-
-import { call, put } from "redux-saga/effects";
+import {
+      call,
+      put
+} from "redux-saga/effects";
 import APILayer from "../apis/index";
 
 
 
 export function* layerFetchList(action) {
-    const response = yield call(APILayer.getAll);
-    const payload = response ? response.data : {}
-    yield put({ type: 'LAYER_FETCH', payload });
+
+
+      const response = yield call(APILayer.getAll);
+      const payload = response ? response.data : {}
+      yield put({
+            type: 'LAYER_FETCH',
+            payload
+      });
 }
 
 
 export function* layerAdd(action) {
-    
-    yield call(APILayer.add, action.payload);
-    yield put({type: 'LAYER_ADD', layer: action.payload});
-    action.callbackSuccess();
+
+      try {
+            yield call(APILayer.add, action.payload);
+            yield put({
+                  type: 'LAYER_ADD',
+                  layer: action.payload
+            });
+            // return;
+            action.callbackSuccess();
+      } catch (e) {
+            console.log(e)
+      }
 }
