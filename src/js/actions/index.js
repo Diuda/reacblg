7a3494ac34657f459ee4434574183ccb5e53721a
Diuda <divyanshu.1996@gmail.com>
2019-01-11 19:23:58 +0530

    calling dispatcher changed

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,7 +1,7 @@
-export const addProduct = product =>({
-    type: "ADD_PROD", payload: product
-});
+// export const addProduct = product =>({
+//     type: "ADD_PROD", payload: product
+// });
 
 export const addProductAPI = product =>({
-    type: "LAYER_ADD", payload: product
+    type: "LAYER_ADD_SUCCESS", payload: product
 });
